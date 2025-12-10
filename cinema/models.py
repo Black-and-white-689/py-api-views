b3d9f1871385cda7e2b4eaaa80eb0fd164c336f3
Ihor Pershin <issue.07.08.09@gmail.com>
2025-12-10 22:05:49 +0200

    'Solution'

--- cinema/models.py
+++ cinema/models.py
@@ -1,10 +1,48 @@
 from django.db import models
 
+from django.db.models import ForeignKey
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField(
+        "Actor",
+        related_name="movies",
+        verbose_name="actor"
+    )
+    genres = models.ManyToManyField(
+        "Genre",
+        null=True,
+        blank=True
+    )
     duration = models.IntegerField()
 
     def __str__(self):
         return self.title
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+
+    def __str__(self):
+        return (f"Name: {self.name} "
+                f"(Rows: {self.rows}, "
+                f"Seats in rows: {self.seats_in_row})")
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,26 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from cinema.views import movie_list, movie_detail
+from .views import (
+    GenreList,
+    GenreDetail,
+    ActorListCreateView,
+    ActorDetailView,
+    CinemaHallViewSet,
+    MovieViewSet,
+)
 
-urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
-]
 
 app_name = "cinema"
+
+router = DefaultRouter()
+router.register(r"cinema_halls", CinemaHallViewSet, basename="cinema_hall")
+router.register(r"movies", MovieViewSet, basename="movie")
+
+urlpatterns = [
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorListCreateView.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetailView.as_view(), name="actor-detail"),
+    path("", include(router.urls)),
+]
